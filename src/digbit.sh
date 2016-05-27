31c0bafdb5ddb118437632847b1585399d3eba19
Ron Stoner <stonroner@gmail.com>
2016-05-27 12:02:42 -0400

    Read from list instead of stdin
    Updated script to read from pre-generated domain list instead of stdin.  Script outputs an integer to stdout which can be passed to NOC dashboards and monitoring solutions.

--- src/digbit.sh
+++ src/digbit.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
- 
-for i in $( ./digbit.py $1);
-do
-        dotcheck=$( echo "$i" | grep "\." | wc -l)
-        echo -n "$i ";
+set regcount = 0
+filename='permutations.txt'
+filelines=`cat $filename`
+
+for i in $filelines ; do 
+        
+	dotcheck=$( echo "$i" | grep "\." | wc -l)
         check=$(nslookup $i| grep "NXDOMAIN" | wc -l);
         if [[ $check -ne 0 && dotcheck -ne 0 ]];
-        then
-                echo " < available";
+        	then
+ 	                :
         else
-                echo " ";
-        fi;
+		((regcount++));
+	fi;
 done
+echo $regcount;
